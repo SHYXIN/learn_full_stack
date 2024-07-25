ba96a3abafcd4f25f0676f77d3c2e3c3255f0a86
Py_TaiYuan_WangXin <wangxin@zhulong.com.cn>
2024-07-25 09:46:41 +0800

    第9章 完成评论喜欢功能

--- core/post/viewsets.py
+++ core/post/viewsets.py
@@ -38,7 +38,7 @@ class PostViewSet(AbstractViewSet):
         post = self.get_object()
         user = self.request.user
 
-        user.like(post)
+        user.like_post(post)
 
         serializer = self.serializer_class(post)
 
@@ -49,7 +49,7 @@ class PostViewSet(AbstractViewSet):
         post = self.get_object()
         user = self.request.user
 
-        user.remove_like(post)
+        user.remove_like_post(post)
 
         serializer = self.serializer_class(post)
 
