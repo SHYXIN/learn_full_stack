8d397abf8e14eb765d55582952e30c533550d1dd
Py_TaiYuan_WangXin <wangxin@zhulong.com.cn>
2024-08-01 11:37:13 +0800

    第16章 结束

--- CoreRoot/settings.py
+++ CoreRoot/settings.py
@@ -200,3 +200,17 @@ MEDIA_ROOT = BASE_DIR / 'uploads'
 # Default avatar URL
 # DEFAULT_AVATAR_URL = "https://avatars.dicebear.com/api/identicon/.svg"
 DEFAULT_AVATAR_URL = "https://api.dicebear.com/9.x/identicon/svg"
+
+
+# Caching
+
+CACHES = {
+    "default": {
+        "BACKEND": "django_redis.cache.RedisCache",
+        "LOCATION": "redis://127.0.0.1:6379/1",
+        # "LOCATION": "redis://redis:6379",
+        "OPTIONS": {
+            "CLIENT_CLASS": "django_redis.client.DefaultClient",
+        }
+    }
+}
